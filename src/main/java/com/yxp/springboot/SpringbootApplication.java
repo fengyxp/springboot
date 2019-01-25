adc92aa0d7c5363c10afc2872471385be4ec27e9
fengyxp <1262597618@qq.com>
2019-01-25 17:52:20 +0800

    fd

--- src/main/java/com/yxp/springboot/SpringbootApplication.java
+++ src/main/java/com/yxp/springboot/SpringbootApplication.java
@@ -3,6 +3,8 @@ package com.yxp.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.yxp.springboot.dao")
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringApplication.class);
     }
 
 }
